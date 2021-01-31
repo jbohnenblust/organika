baf1282f97ea3f58285e4e6f3dd190fb9c20e938
Jeff <jb198720131@gmail.com>
2021-01-31 12:59:44 -0600

    initial commit

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    
+    render() { 
+        return (  
+            <center>Some more text goes here</center>
+        );
+    }
+}
+ 
+export default Footer;
---
+++ src/components/navBar/navBar.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'; //imrc
+
+class NavBar extends Component {
+    
+    render() { 
+        return (  
+            <div>
+                <h1>Menu here later!</h1>
+                <hr></hr>
+                And then some text on the bottom of the navBar comp
+            </div>
+        );
+    }
+}
+ 
+export default NavBar;
